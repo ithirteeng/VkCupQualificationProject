f8a1a360a8c8aff07ce6b353dc44029ac97d9cad
ithirteeng <ithirteengw@gmail.com>
2022-12-12 16:53:44 +0700

    added animation on clicking the category

--- app/src/main/java/com/example/vkcupqualificationproject/CategoryCustomView.kt
+++ app/src/main/java/com/example/vkcupqualificationproject/CategoryCustomView.kt
@@ -1,6 +1,8 @@
 package com.example.vkcupqualificationproject
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +17,23 @@ class CategoryCustomView @JvmOverloads constructor(
 	private val categoryView: View = LayoutInflater.from(context).inflate(R.layout.custom_view_category, this)
 	private val binding = CustomViewCategoryBinding.bind(categoryView)
 
+	private var tick = false
 
+	@SuppressLint("UseCompatLoadingForDrawables")
+	private fun addAnimation() {
+		val tickToAdd: AnimatedVectorDrawable = this.resources.getDrawable(R.drawable.avd_tick_to_add, context.theme) as AnimatedVectorDrawable
+		val addToTick: AnimatedVectorDrawable = this.resources.getDrawable(R.drawable.avd_add_to_tick, context.theme) as AnimatedVectorDrawable
+		val drawable: AnimatedVectorDrawable = if (tick) tickToAdd else addToTick
+		binding.imageButton.setImageDrawable(drawable)
+		drawable.start()
+		tick = !tick
+	}
+
+	fun onButtonClickListener() {
+		binding.imageButton.setOnClickListener {
+			addAnimation()
+		}
+	}
 
 
 }
